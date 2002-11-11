2e37cf61bbbad08b9cff81da2ca9976b81f15e5f
Trent Jarvi <rxtx@qbang.org>
2002-11-11 18:12:43 +0000

    version++

--- src/SerialImp.c
+++ src/SerialImp.c
@@ -3843,7 +3843,7 @@ RXTXCommDriver.nativeGetVersion
 JNIEXPORT jstring JNICALL RXTXCommDriver(nativeGetVersion) (JNIEnv *env,
 	jclass jclazz )
 {
-	return (*env)->NewStringUTF( env, "RXTX-2.1-7pre6 (Lions and Tigers and Bears)" );
+	return (*env)->NewStringUTF( env, "RXTX-2.1-7pre7" );
 }
 
 /*----------------------------------------------------------
