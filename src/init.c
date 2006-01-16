5d31f14f38b2768232d5675ab56fa90597b36cdf
Trent Jarvi <rxtx@qbang.org>
2006-01-16 02:55:56 +0000

    JNI_OnLoad and JNI_OnUnload are in SerialImp.c now so remove                 them from init.c                 Gerrit Telkamp g.telkamp@domologic.de                 http://mailman.qbang.org/pipermail/rxtx/20051230/002018.html

--- src/init.c
+++ src/init.c
@@ -17,8 +17,12 @@ BOOL WINAPI __declspec(dllexport) LibMain(HINSTANCE hDLLInst, DWORD fdwReason, L
 /**
  * This is the standart implementation of Java 2 OnLoad and OnUnload native
  * library calls. This template defines them empty functions
+ *
+ * Now in SerialImp.c
  */
+/*
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *reserved)   { return JNI_VERSION_1_2; }
 JNIEXPORT void JNICALL JNI_OnUnload(JavaVM *vm, void *reserved) {}
+*/
 
 #endif
