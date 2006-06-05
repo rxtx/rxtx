eeb455e8393581730f66382cfecbf240430baaef
Trent Jarvi <rxtx@qbang.org>
2006-06-05 15:16:13 +0000

    updated XCode project (non-compressed)

---
+++ MACOSX_IDE/XCode/config.h
@@ -0,0 +1,35 @@
+/* Define if you have the <fcntl.h> header file.  */
+#define HAVE_FCNTL_H 1
+
+/* Define if you have the <grp.h> header file.  */
+#define HAVE_GRP_H 1
+
+/* Define if you have the <pwd.h> header file.  */
+#define HAVE_PWD_H 1
+
+/* Define if you have the <signal.h> header file.  */
+#define HAVE_SIGNAL_H 1
+
+/* Define if you have the <sys/fcntl.h> header file.  */
+#define HAVE_SYS_FCNTL_H 1
+
+/* Define if you have the <sys/file.h> header file.  */
+#define HAVE_SYS_FILE_H 1
+
+/* Define if you have the <sys/signal.h> header file.  */
+#define HAVE_SYS_SIGNAL_H 1
+
+/* Define if you have the <sys/time.h> header file.  */
+#define HAVE_SYS_TIME_H 1
+
+/* Define if you have the <termios.h> header file.  */
+#define HAVE_TERMIOS_H 1
+
+/* Name of package */
+#define PACKAGE "Serial"
+
+/* Version number of package */
+#define VERSION "2.1-8"
+
+//#define DEBUG
+//#define DEBUG_VERBOSE
